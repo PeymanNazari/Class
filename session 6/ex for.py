42f0e15a7f2a3058e3ccbcda343c884bbf279857
PeymanNazari <148754935+PeymanNazari@users.noreply.github.com>
2023-11-06 23:36:50 +0330

    Update ex for.py

--- session 6/ex for.py
+++ session 6/ex for.py
@@ -1,9 +1,11 @@
-for student in range(10):
-    student=(input()) # the name of student
-    score=(int(input()))
-    if score >= 10:
-      print(student, "with ","score",score, " is accepted")
-    else:
+for student in range(3):
+    student=(input()) # the name of the student
+    score=(int(input())) # the score of the student
+    if score > 20 :
+       print("you typed wrong number")
+    if (score >= 10 and score <=20) :
+      print(student, "with ","score",score, " is accepted") 
+    if score < 10 :
        print(student,"with","score",score,"is failed")
 print("end of score table")
     
