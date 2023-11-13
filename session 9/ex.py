5b6e7b0aa8a0ea0d1f58a23148117ff10c27a425
PeymanNazari <148754935+PeymanNazari@users.noreply.github.com>
2023-11-13 13:21:03 +0330

    Add files via upload

---
+++ session 9/ex.py
@@ -0,0 +1,21 @@
+number1=int(input())#Enter number
+number2=int(input())#Enter number
+number3=int(input())#Enter number
+number4=int(input())#Enter number
+number5=int(input())#Enter number
+number6=int(input())#Enter number
+number7=int(input())#Enter number
+number8=int(input())#Enter number
+number9=int(input())#Enter number
+number10=int(input())#Enter number
+print([number1,number2,number3,number4,number5,number6,number7,number8,number9,number10])
+list=[number1,number2,number3,number4,number5,number6,number7,number8,number9,number10]
+Minimum=int(input())
+print("Minimum is",Minimum)
+Maximum=int(input())
+print("Maximum is",Maximum)
+print(((list[0]+list[1]+list[2]+list[3]+list[4]+list[5]+list[6]+list[7]+list[8]+list[9]))/2, "is average")
+ave=float(input())
+print(((list[0]-ave)**2)+((list[1]-ave)**2)+((list[2]-ave)**2)+((list[3]-ave)**2)+((list[4]-ave)**2)
+      +((list[5]-ave)**2)+((list[6]-ave)**2)+((list[7]-ave)**2)+((list[8]-ave)**2)+((list[9]-ave)**2),"as VAR")
+
