e9eea6891aacc56ae9c2766c94a897fb5b034180
PeymanNazari <148754935+PeymanNazari@users.noreply.github.com>
2023-11-13 13:49:48 +0330

    Add files via upload

---
+++ session 9/ex-.py
@@ -0,0 +1,22 @@
+number1=int(input())#Enter number
+number2=int(input())#Enter number
+number3=int(input())#Enter number
+number4=int(input())#Enter number
+number5=int(input())#Enter number
+number6=int(input())#Enter number
+number7=int(input())#Enter number
+number8=int(input())#Enter number
+number9=int(input())#Enter number
+number10=int(input())#Enter number
+print(number1,number2,number3,number4,number5,number6,number7,number8,number9,number10)
+
+Minimum=int(input())
+print("Minimum is",Minimum)
+Maximum=int(input())
+print("Maximum is",Maximum)
+
+print("average is",(number1+number2+number3+number4+number5+number6+number7+number8+number9+number10)/2)
+ave=float(input())
+
+print("VAR is",(((number1)-ave)**2)+(((number2)-ave)**2)+(((number3)-ave)**2)+(((number4)-ave)**2)+(((number5)-ave)**2)
+      +(((number6)-ave)**2)+((number7-ave)**2)+((number8-ave)**2)+((number9-ave)**2)+(((number10-ave)**2)))
