9c9c9f7a302335277be1737b8719acd3c30dafb9
PeymanNazari <148754935+PeymanNazari@users.noreply.github.com>
2023-11-19 09:10:01 +0330

    Update s.p.s.py

--- session 11/s.p.s.py
+++ session 11/s.p.s.py
@@ -1,8 +1,34 @@
 import random
+OPTIONS=["scissors","paper","stone"]
+cpu=random.choice(OPTIONS)
+print("type only 1 item from these words: scissors,paper,stone")
+player=input("choose from: scissors,paper,stone")
 
-n1 = random.randint(1, 10)
-
-print(n1)
-
-s1 = random.choice(['a', 'b', 'c'])
-print(s1)
+if player == "scissors":
+ if cpu == "paper":
+     print("You won")
+     if cpu == "stone":
+      print("You lose")
+      if cpu == "scissors" :
+       print=("Draw")
+      else:
+        print("you entered wrong word")
+if player == "stone" :  
+      if cpu == "paper" :
+       print( "You lose")  
+      if cpu == "scissors":
+       print("You lose")
+      if cpu == "stone":
+       print("DRAW")  
+      else:
+       print("you entered wrong word")
+if player == "paper":
+      if cpu == "paper" :
+       print ("DRAW")
+       if cpu == "stone":
+        print("You won")
+      if cpu == "scissors":
+       print("You lose")
+      else:
+       print("you entered wrong word")
+print("Rematch?")    
