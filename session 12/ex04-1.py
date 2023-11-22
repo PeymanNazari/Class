2a9d7af8bcdc4236c1025a4ab0860a70302fea4f
PeymanNazari <148754935+PeymanNazari@users.noreply.github.com>
2023-11-22 11:38:15 +0330

    Add files via upload

---
+++ session 12/ex04-1.py
@@ -0,0 +1,11 @@
+XM=[[3,5,9],[1,0,2],[0,0,-1]]#X matris
+YM=[[1,2,3],[9,5,-3],[2,-1,7]]# Y matris
+print("sum for matris BM is  :")
+print([XM[0][0]+YM[0][0],XM[0][1]+YM[0][1],XM[0][2]+YM[0][2]])
+print([XM[1][0]+YM[1][0],XM[1][1]+YM[1][1],XM[1][2]+YM[1][2]])
+print([XM[2][0]+YM[2][0],XM[2][1]+YM[2][1],XM[2][2]+YM[2][2]]) 
+
+print("multi for matris BM is  :")
+print([XM[0][0]*YM[0][0]+XM[0][1]*YM[1][0]+XM[0][2]*YM[2][0]])
+print([XM[1][0]*YM[0][1]+XM[1][1]*YM[1][1]+XM[2][1]*YM[2][1]])
+print([XM[2][0]*YM[0][2]+XM[2][1]*YM[2][2]+XM[2][2]*YM[2][2]]) 
